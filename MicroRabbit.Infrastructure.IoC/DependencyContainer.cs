5174bde4f2c08f29158f7118da1230dfd54b3120
Nick Qantaria <skott56@outlook.com>
2021-02-07 19:32:25 +0400

    Initial commit of DependencyContainer

---
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
